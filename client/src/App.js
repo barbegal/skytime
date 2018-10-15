3af434bf275be170400caacab2dd13bc3c9d6f55
Dan <dan@joincad.com>
2018-10-14 22:43:09 -0700

    Meta for web image

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import './App.css';
 class App extends Component {
   render() {
     // img file here -- replace every few seconds
+    // 96 images over... try 6... divide and blend
+    //from 0 to X, add image 0, from 1 to 2 add image 2, etc etc.
     return (
       <div className="App">
       
